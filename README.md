bdb3fcf1be352783043e6c09881ecdd63d140f99
István Svorcz <walakymas@gmail.com>
2022-10-12 10:04:46 +0200

    refactor

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ hogy egyszerűbben futtatható legyen.
 #Rendszer beállítások:
 
 - HOOK: Webhook Url alapértelmezés nincs, kötelezően megadni
-- TEAM: Mely csoportok helyettesítéseiről küldjön üzenetet (default: '09...xd[^2]' ami a 9D 'Észak')
+- TEAM: Mely csoportok helyettesítéseiről küldjön üzenetet (default: '09(...xd|(an|ol|ne).x)' ami 9. évfolyam d vagy nyelvi )
 - SOURCE: Honnan vegye a html-t (default: 'https://www.ejg.hu/helyettes/', teszteléskor egy 
   mentett példányt használok 'file:///home/######/helyettes.html' url-l)
 
 # Crontab beállítások minta
 ```
+VIBER= #viber channel#
 HOOK= #WebhookUrl#
 */10 * * * * ${PATHTOSCRIPT}/missing.py > missing.log
 ```
 
 # TODO
-
+- regexp pontosítás
 - Több webhook és minta kezelése?
 - Futtatás Windows környezetben?
--- missing.py
+++ missing.py
@@ -8,6 +8,7 @@ from os import environ
 
 hook = environ.get('HOOK')
 vibea = environ.get('VIBER')
+#team = environ.get('TEAM',"09(...xd|(an|ol|ne).x)")
 team = environ.get('TEAM',"09(...xd|(an|ol|ne).xcdf)")
 link = environ.get('SOURCE',"https://www.ejg.hu/helyettes/")
 
@@ -45,8 +46,117 @@ orak = {
     'tor': 'Történelem',
     'ola': 'Olasz',
     'ang': 'Angol',
-    'nem': 'Német'
+    'nem': 'Német',
+    'foc': 'Földrajz'
 }
+
+class Ora:
+    def __init__(self, match):
+        self.datum = match.group(1)
+        self.tanar = match.group(2)
+        self.terem = match.group(3)
+        self.nap = napok[match.group(3)[0:1]]
+        self.csoport = match.group(4)
+        self.megjegyzes = match.group(7)
+        self.helyettes = match.group(6).replace("&nbsp","")
+        if self.helyettes=='':
+            self.helyettes = 'Nincs'
+
+        self.targy = orak.get(match.group(5)[0:3], match.group(5)[0:3])
+
+        if self.csoport[7]=='1':
+            self.team = 'Észak'
+        elif self.csoport[7]=='2':
+            self.team = 'Dél'
+        elif self.csoport[2:10]=='tesxdfno':
+            self.team = 'Lány'
+        elif self.csoport[2:10]=='tesxdfff':
+            self.team = 'Fiú'
+        else :
+            self.team = 'Osztály'
+
+
+        if "online" in match.group(7):
+            self.title = "Online"
+            self.emoji = ""
+            self.color = 2135072
+        elif match.group(6) == "&nbsp":
+            self.title = "Elmarad"
+            self.emoji = ""
+            self.color = 15844367
+        else:
+            self.title = "Helyettesítés"
+            self.emoji = ""
+            self.color = 2123412
+
+    def need(self):
+        return re.match(team, self.csoport) and self.megjegyzes!='-megtartja'
+
+class Viber:
+    def send(self, ora:Ora):
+        if vibea or debug:
+            vibe = {
+                "auth_token":vibea,
+                "from": "3QbOontxw2C2huap/Lclww==",
+                "type": "text",
+                "text": f"""*{ora.title}* {ora.team} {ora.targy} {ora.nap} {match.group(3)[1:2]}. óra
+Tanár: {ora.tanar}
+Helyettes: {ora.helyettes}
+{ora.datum} {ora.terem} {ora.megjegyzes}"""
+            }
+            v = json.dumps(vibe, indent=2);
+            jsondataasbytes = v.encode('utf-8')
+
+            if debug:
+                print(v)
+            else:
+                req = Request(
+                    'https://chatapi.viber.com/pa/post',
+                    headers={
+                        "Content-Type": "application/json",
+                        "Content-Length": len(jsondataasbytes)
+                    },
+                    data=jsondataasbytes,
+                    method="POST"
+                )
+                urlopen(req)
+
+
+class Discord:
+    def send(self, ora:Ora):
+        if hook!="missing" or debug:
+            e = json.dumps({"embeds": [{
+                "title": ora.title,
+                "description": ora.datum + " " + ora.terem[0:2] + " " + ora.team + " " + ora.targy + " " +  ora.megjegyzes,
+                "type": "rich",
+                "color": ora.color,
+                "fields": [
+                    {"name": "Dátum", "value":ora.datum,"inline":"true"},
+                    {"name": "Tanár", "value":ora.tanar,"inline":"true"},
+                    {"name": "Terem", "value":ora.terem,"inline":"true"},
+                    {"name": "Csoport", "value":ora.csoport,"inline":"true"},
+                    {"name": "Tantárgy", "value":ora.targy,"inline":"true"},
+                    {"name": "Helyettes", "value":ora.helyettes,"inline":"true"}
+                ],
+                "footer" : {"text":"E5vös helyettesítés info v0.1.2"}
+            }]}, indent=2)
+            jsondataasbytes = e.encode('utf-8')
+            if debug:
+                print(e)
+            else:
+                req = Request(
+                    hook,
+                    headers={
+                        "Content-Type": "application/json",
+                        "User-Agent": "Python",
+                        "Content-Length": len(jsondataasbytes)
+                    },
+                    data=jsondataasbytes,
+                    method="POST"
+                )
+                urlopen(req)
+
+
 
 sent = []
 newsent = []
@@ -62,106 +172,23 @@ for sor in myfile.splitlines():
     match = re.match("<TR><TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<td[^>]*>([^<]+)<\/td> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<.",sor)
 
     if match :
-        if re.match(team, match.group(4)):
-            if debug:
-                print(match.group(4))
-                print( match.group(7))
-                print("online" in match.group(7))
+        ora = Ora(match)
+        if ora.need():
             key = match.group(1)+":"+match.group(3)
-
             newsent.append(key)
-
-            if match.group(4)[7]=='1':
-                t = 'Észak'
-            elif match.group(4)[7]=='2':
-                t = 'Dél'
-            else:
-                t = 'Osztály'
 
             if key in sent:
                 print(f"Already sent {key}")
                 if not debug:
                     continue
 
-            if match.group(6)=='-megtartja':
-                continue
+            Discord().send(ora)
+            Viber().send(ora)
 
-            helyettes = match.group(6).replace("&nbsp","")
-            if helyettes=='':
-                helyettes = 'Nincs'
+if not debug:
+    f = open("sent.json", "w")
+    f.write(json.dumps(newsent, indent=2))
+    f.close()
+else:
+    print(json.dumps(newsent, indent=2))
 
-            targy = orak.get(match.group(5)[0:3], match.group(5)[0:3])
-
-            embed = {
-                "title": "Helyettesítés",
-                "description": match.group(1) + " " + match.group(3)[0:2] + " " + t + " " + targy + " " +  match.group(6).replace("&nbsp",""),
-                "type": "rich",
-                "color": 2123412,
-                "fields": [
-                    {"name": "Dátum", "value":match.group(1),"inline":"true"},
-                    {"name": "Tanár", "value":match.group(2),"inline":"true"},
-                    {"name": "Terem", "value":match.group(3),"inline":"true"},
-                    {"name": "Csoport", "value":match.group(4),"inline":"true"},
-                    {"name": "Tantárgy", "value":targy,"inline":"true"},
-                    {"name": "Helyettes", "value":helyettes,"inline":"true"}
-                ],
-                "footer" : {"text":"E5vös helyettesítés info v0.1.2"}
-            }
-
-            if "online" in match.group(7):
-                embed["title"] = "Online"
-                embed["color"] = 2135072
-            elif match.group(6) == "&nbsp":
-                embed["title"] = "Elmarad"
-                embed["color"] = 15844367
-
-            e = json.dumps({"embeds": [embed]}, indent=2)
-
-            if hook!="missing" or debug:
-                jsondataasbytes = e.encode('utf-8')
-                if debug:
-                    print(e)
-                else:
-                    req = Request(
-                        hook,
-                        headers={
-                            "Content-Type": "application/json",
-                            "User-Agent": "Python",
-                            "Content-Length": len(jsondataasbytes)
-                        },
-                        data=jsondataasbytes,
-                        method="POST"
-                    )
-                    urlopen(req)
-
-            if vibea or debug:
-                vibe = {
-                    "auth_token":vibea,
-                    "from": "3QbOontxw2C2huap/Lclww==",
-                    "type": "text",
-                    "text": f"""*{embed['title']}* {t} {napok[match.group(3)[0:1]]} {match.group(3)[1:2]}. óra
-    _{targy}_ {match.group(2)}
-    Helyettes: {helyettes}
-    {match.group(1)} {match.group(3)} {match.group(6).replace("&nbsp","")}"""
-                }
-                v = json.dumps(vibe, indent=2);
-                jsondataasbytes = v.encode('utf-8')
-
-                if debug:
-                    print(v)
-                else:
-                    req = Request(
-                        'https://chatapi.viber.com/pa/post',
-                        headers={
-                            "Content-Type": "application/json",
-                            "Content-Length": len(jsondataasbytes)
-                        },
-                        data=jsondataasbytes,
-                        method="POST"
-                    )
-                    urlopen(req)
-
-    if not debug:
-        f = open("sent.json", "w")
-        f.write(json.dumps(newsent, indent=2))
-        f.close()
