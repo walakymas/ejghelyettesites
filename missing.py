a1dc10200f7a504d7331406c772f13560d708d7c
István Svorcz <walakymas@gmail.com>
2022-10-12 07:20:31 +0200

    detailed info debug pattern refactor

--- missing.py
+++ missing.py
@@ -1,86 +1,72 @@
 #!/usr/bin/env python
-
-from flask import Flask, request, Response
-from viberbot import Api
-from viberbot.api.bot_configuration import BotConfiguration
-from viberbot.api.messages.text_message import TextMessage
-from viberbot.api.viber_requests import ViberConversationStartedRequest
-from viberbot.api.viber_requests import ViberFailedRequest
-from viberbot.api.viber_requests import ViberMessageRequest
-from viberbot.api.viber_requests import ViberSubscribedRequest
-from viberbot.api.viber_requests import ViberUnsubscribedRequest
-
-import time
-import logging
-import sched
-import threading
+import sys
 from urllib.request import urlopen, Request
 import re
 import json
 from os.path import exists
 from os import environ
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-app = Flask(__name__)
-
-@app.route('/', methods=['POST'])
-def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
-    viber_request = viber.parse_request(request.get_data())
-    # handle the request here
-    if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        # lets echo back
-        logger.info(message.sender.id)
-    return Response(status=200)
-
-hook = environ.get('HOOK', "missing")
-vibera = environ.get('VIBER', "missing")
-team = environ.get('TEAM',"09...xd")
+hook = environ.get('HOOK')
+vibea = environ.get('VIBER')
+team = environ.get('TEAM',"09(...xd|(an|ol|ne).xcdf)")
 link = environ.get('SOURCE',"https://www.ejg.hu/helyettes/")
 
-bot_configuration = BotConfiguration(
-    name='EjgHelyettesites',
-    avatar='https://www.ejg.hu/e5logo.png',
-    auth_token=vibera
-)
-viber = Api(bot_configuration)
+debug = not (vibea or hook)
 
-print(hook)
-print(team)
+if debug:
+    print("DEBUG MODE")
 
+napok = {
+    'h': 'Hétfő',
+    'k': 'Kedd',
+    's': 'Szerda',
+    'c': 'Csütörtök',
+    'p': 'Péntek',
+    'v': 'Vasárnap'
+}
 
-def set_webhook(viber):
-    viber.set_webhook('https://svohome.duckdns.org/ejgtest/')
-    aci = viber.get_account_info();
-    logger.info('')
-    text_message = TextMessage(text="sample text message!")
-    viber.send_messages('SzRDG8vfuQ4RCFYKHbEMJQ==', [
-        text_message
-    ])
+orak = {
+    'ird': 'Irodalom',
+    'kem': 'Kémia',
+    'an1': 'Angol1',
+    'an2': 'Angol2',
+    'ne1': 'Német1',
+    'ne2': 'Német2',
+    'ol1': 'Olasz1',
+    'ol2': 'Olasz2',
+    'bio': 'Biológia',
+    'nyt': 'Nyelvtan',
+    'mat': 'Matematika',
+    'zen': 'Zene',
+    'dig': 'Digitális kultúra',
+    'fiz': 'Fizika',
+    'ofn': 'Osztályfőnöki',
+    'tes': 'Testnevelés',
+    'tor': 'Történelem',
+    'ola': 'Olasz',
+    'ang': 'Angol',
+    'nem': 'Német'
+}
 
-def parse():
+sent = []
+newsent = []
+if (exists("sent.json")):
+    f = open("sent.json", "r")
+    sent = json.loads(f.read())
 
-    sent = []
-    newsent = []
-    if (exists("sent.json")):
-        f = open("sent.json", "r")
-        sent = json.loads(f.read())
+f = urlopen(link)
+myfile = f.read().decode('utf-8')
+myfile = re.sub(">\n",">",myfile)
+myfile = re.sub("><TR>",">\n<TR>",myfile)
+for sor in myfile.splitlines():
+    match = re.match("<TR><TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<td[^>]*>([^<]+)<\/td> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<.",sor)
 
-    f = urlopen(link)
-    myfile = f.read().decode('utf-8')
-    myfile = re.sub(">\n",">",myfile)
-    myfile = re.sub("><TR>",">\n<TR>",myfile)
-    for sor in myfile.splitlines():
-        match = re.match("<TR><TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>("+team+"[^<]+)<\/TD> *<td[^>]*>([^<]+)<\/td> *<TD[^>]*>([^<]+)<\/TD> *<TD[^>]*>([^<]+)<.",sor)
-
-        if match:
+    if match :
+        if re.match(team, match.group(4)):
+            if debug:
+                print(match.group(4))
+                print( match.group(7))
+                print("online" in match.group(7))
             key = match.group(1)+":"+match.group(3)
 
             newsent.append(key)
@@ -94,11 +80,21 @@ def parse():
 
             if key in sent:
                 print(f"Already sent {key}")
+                if not debug:
+                    continue
+
+            if match.group(6)=='-megtartja':
                 continue
+
+            helyettes = match.group(6).replace("&nbsp","")
+            if helyettes=='':
+                helyettes = 'Nincs'
+
+            targy = orak.get(match.group(5)[0:3], match.group(5)[0:3])
 
             embed = {
                 "title": "Helyettesítés",
-                "description": match.group(1) + " " + match.group(3)[0:2] + " " + t + " " + match.group(5) + " " +  match.group(7),
+                "description": match.group(1) + " " + match.group(3)[0:2] + " " + t + " " + targy + " " +  match.group(6).replace("&nbsp",""),
                 "type": "rich",
                 "color": 2123412,
                 "fields": [
@@ -106,39 +102,66 @@ def parse():
                     {"name": "Tanár", "value":match.group(2),"inline":"true"},
                     {"name": "Terem", "value":match.group(3),"inline":"true"},
                     {"name": "Csoport", "value":match.group(4),"inline":"true"},
-                    {"name": "Tantárgy", "value":match.group(5),"inline":"true"},
-                    {"name": "Helyettes", "value":match.group(6).replace("&nbsp","-"),"inline":"true"}
+                    {"name": "Tantárgy", "value":targy,"inline":"true"},
+                    {"name": "Helyettes", "value":helyettes,"inline":"true"}
                 ],
-                "footer" : {"text":"E5vös helyettesítés info v0.1.1"}
+                "footer" : {"text":"E5vös helyettesítés info v0.1.2"}
             }
 
-            if match.group(6) == "&nbsp":
-                embed["title"] = "Elmarad :heart_eyes:"
-                embed["color"] = 15844367;
+            if "online" in match.group(7):
+                embed["title"] = "Online"
+                embed["color"] = 2135072
+            elif match.group(6) == "&nbsp":
+                embed["title"] = "Elmarad"
+                embed["color"] = 15844367
 
-            e = json.dumps({"embeds": [embed]}, indent=2);
-            jsondataasbytes = e.encode('utf-8')
-            req = Request(
-                hook,
-                headers={
-                    "Content-Type": "application/json",
-                    "User-Agent": "",
-                    "Content-Length": len(jsondataasbytes)
-                },
-                data=jsondataasbytes,
-                method="POST"
-            )
-            urlopen(req, jsondataasbytes)
+            e = json.dumps({"embeds": [embed]}, indent=2)
 
+            if hook!="missing" or debug:
+                jsondataasbytes = e.encode('utf-8')
+                if debug:
+                    print(e)
+                else:
+                    req = Request(
+                        hook,
+                        headers={
+                            "Content-Type": "application/json",
+                            "User-Agent": "Python",
+                            "Content-Length": len(jsondataasbytes)
+                        },
+                        data=jsondataasbytes,
+                        method="POST"
+                    )
+                    urlopen(req)
+
+            if vibea or debug:
+                vibe = {
+                    "auth_token":vibea,
+                    "from": "3QbOontxw2C2huap/Lclww==",
+                    "type": "text",
+                    "text": f"""*{embed['title']}* {t} {napok[match.group(3)[0:1]]} {match.group(3)[1:2]}. óra
+    _{targy}_ {match.group(2)}
+    Helyettes: {helyettes}
+    {match.group(1)} {match.group(3)} {match.group(6).replace("&nbsp","")}"""
+                }
+                v = json.dumps(vibe, indent=2);
+                jsondataasbytes = v.encode('utf-8')
+
+                if debug:
+                    print(v)
+                else:
+                    req = Request(
+                        'https://chatapi.viber.com/pa/post',
+                        headers={
+                            "Content-Type": "application/json",
+                            "Content-Length": len(jsondataasbytes)
+                        },
+                        data=jsondataasbytes,
+                        method="POST"
+                    )
+                    urlopen(req)
+
+    if not debug:
         f = open("sent.json", "w")
         f.write(json.dumps(newsent, indent=2))
         f.close()
-
-if __name__ == "__main__":
-    scheduler = sched.scheduler(time.time, time.sleep)
-    scheduler.enter(5, 1, set_webhook, (viber,))
-    t = threading.Thread(target=scheduler.run)
-    t.start()
-
-    app.run(host='0.0.0.0', port=8888, debug=True)
-
